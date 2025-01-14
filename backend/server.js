e95c7ca6978b25fbbac178f7b7c6c831921b3c9d
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-01-14 17:23:26 +0545

    user routes and controllers added

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,36 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 import dotenv from "dotenv";
-import connectMongoDb from "./db/connectMongoDb.js";
-
+import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
+import userRoutes from "./routes/user.routes.js";
+import {v2 as cloudinary} from "cloudinary";
 dotenv.config();
+
+
+cloudinary.config();({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 const app = express();
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 // console.log(process.env.MONGO_URI)
 
+
+app.use(express.json());    //middleware to parse req. body
+app.use(express.urlencoded({extended: true}));  //to parse form data
+
+app.use(cookieParser());
+
 app.use("/api/auth",authRoutes);
+app.use("/api/users",userRoutes);
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`);
-    connectMongoDb();
+    // console.log(`Server is running on port 5000`);
+    connectMongoDB();
 });
